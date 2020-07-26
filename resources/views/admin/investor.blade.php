9215c2fbace92f0e40e0af1cf6edafda7d03f40e
Mayorwa <mayorviper.mo@gmail.com>
2020-07-26 22:51:38 +0100

    Did the lock for the admin part 😉

--- resources/views/admin/investor.blade.php
+++ resources/views/admin/investor.blade.php
@@ -2,55 +2,55 @@
 @section('content')
     <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor">
         <div class="kt-content  kt-grid__item kt-grid__item--fluid row" id="kt_content">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <div class="kt-portlet">
-                <div class="kt-portlet__head">
-                    <div class="kt-portlet__head-label">
-                        <h3 class="kt-portlet__head-title">
-                            Details for <a style="color: #5867dd; font-weight: bold">{{$gUser->name}}</a>
-                        </h3>
+                    <div class="kt-portlet__head">
+                        <div class="kt-portlet__head-label">
+                            <h3 class="kt-portlet__head-title">
+                                Details for <a style="color: #5867dd; font-weight: bold">{{$gUser->name}}</a>
+                            </h3>
+                        </div>
                     </div>
-                </div>
-                <div class="kt-portlet__body">
-                    <div class="card-text">
-                        <dl class="row text-black">
-                            <dt class="col-md-8">Email Address</dt>
-                            <dd class="col-md-4 text-right">{{ $gUser->email }}</dd>
-                        </dl>
-                        <hr>
-                        <dl class="row text-black">
-                            <dt class="col-md-9">Phone Number</dt>
-                            <dd class="col-md-3 text-right"> {{ $gUser->phone }}</dd>
-                        </dl>
-                        <hr>
-                        <dl class="row text-black">
-                            <dt class="col-md-9">Number of Transactions Made</dt>
-                            <dd class="col-md-3 text-right text-success">{{count($transactions)}}</dd>
-                        </dl>
-                        <hr>
-                        <dl class="row text-black">
-                            <dt class="col-md-9">Stash Balance</dt>
-                            <dt class="col-md-3 text-right text-success" style="font-size: 16px">₦ {{App\Http\Helpers\Formatter::MoneyConvert($stash->availableAmount, "full")}}</dt>
-                        </dl>
-                        <hr>
-                        <dl class="row text-black">
-                            <dt class="col-md-9">Account Details</dt>
-                            <dd class="col-md-3 text-right">
-                                <div class="guarantor">
-                                    <p>BVN: <span>{{strtoupper($account->bvn)}}</span></p>
-                                    <br>
-                                    <p>Account Number: <span>{{$account->accountNumber}}</span></p>
-                                    <br>
-                                    <p>Bank: <span>{{$account->bank->name}}</span></p>
-                                </div>
-                                <hr>
-                            </dd>
-                        </dl>
+                    <div class="kt-portlet__body">
+                        <div class="card-text">
+                            <dl class="row text-black">
+                                <dt class="col-md-8">Email Address</dt>
+                                <dd class="col-md-4 text-right">{{ $gUser->email }}</dd>
+                            </dl>
+                            <hr>
+                            <dl class="row text-black">
+                                <dt class="col-md-9">Phone Number</dt>
+                                <dd class="col-md-3 text-right"> {{ $gUser->phone }}</dd>
+                            </dl>
+                            <hr>
+                            <dl class="row text-black">
+                                <dt class="col-md-9">Number of Transactions Made</dt>
+                                <dd class="col-md-3 text-right text-success">{{count($transactions)}}</dd>
+                            </dl>
+                            <hr>
+                            <dl class="row text-black">
+                                <dt class="col-md-9">Stash Balance</dt>
+                                <dt class="col-md-3 text-right text-success" style="font-size: 16px">₦ {{App\Http\Helpers\Formatter::MoneyConvert($stash->availableAmount, "full")}}</dt>
+                            </dl>
+                            <hr>
+                            <dl class="row text-black">
+                                <dt class="col-md-9">Account Details</dt>
+                                <dd class="col-md-3 text-right">
+                                    <div class="guarantor">
+                                        <p>BVN: <span>{{strtoupper($account->bvn)}}</span></p>
+                                        <br>
+                                        <p>Account Number: <span>{{$account->accountNumber}}</span></p>
+                                        <br>
+                                        <p>Bank: <span>{{$account->bank->name}}</span></p>
+                                    </div>
+                                    <hr>
+                                </dd>
+                            </dl>
+                        </div>
                     </div>
-                </div>
             </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <div class="kt-portlet">
                     <div class="kt-portlet__head">
                         <div class="kt-portlet__head-label">
@@ -67,6 +67,8 @@
                                     <th>Reference</th>
                                     <th>Type</th>
                                     <th>Amount</th>
+                                    <th>Lock Period</th>
+                                    <th>Has Completed</th>
                                     <th>Message</th>
                                     <th>Date</th>
                                 </tr>
@@ -85,6 +87,14 @@
 
                                         <td>{{$investment->unitsBought}}</td>
                                         <td class="text-success">₦ {{App\Http\Helpers\Formatter::MoneyConvert($investment->amount, "full")}}</td>
+                                        <td>{{$investment->period}} Months</td>
+                                        <td>
+                                            @if($investment->isCompleted == false)
+                                                <a class="btn btn-outline-primary btn-sm text-primary">No</a>
+                                            @else
+                                                <a class="btn btn-outline-success btn-sm text-success">Yes</a>
+                                            @endif
+                                        </td>
                                         <td class="text-success">₦ {{App\Http\Helpers\Formatter::MoneyConvert($investment->roi, "full")}}</td>
                                         <td>{{App\Http\Helpers\Formatter::dataTime($investment->datePurchased)}} </td>
                                     </tr>
